366e32d3a4c84777e1813d217eab3ed677e6f3ff
rejaul karim <devrejaul.official@gmail.com>
2024-01-01 13:21:14 +0600

    add

--- app/src/main/java/com/example/kotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/kotlinandroid/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.kotlinandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -19,7 +25,9 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        setContent {
+            Display()
+        }
     }
 }
 
@@ -27,7 +35,49 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, name = "hello", showSystemUi = true)
 @Composable
 fun Display() {
-    ImageCom()
+    TextInput()
+}
+
+
+@Composable
+fun TextInput() {
+    val state = remember { mutableStateOf("") }
+    TextField(
+        value = state.value,
+        onValueChange = {
+            state.value = it
+            Log.i("text_input", it)
+        },
+        label = { Text(text = "Enter Message") },
+        placeholder = { Text(text = "Name") }
+    )
+}
+
+
+@Composable
+fun ButtonCom() {
+    Button(
+        onClick = { }, colors = ButtonDefaults.buttonColors(
+            contentColor = Color.Blue,
+            containerColor = Color.Black
+        ),
+        enabled = true // disable/enabled
+
+    ) {
+        Text(text = "Hello");
+        Image(painter = painterResource(id = R.drawable.image), contentDescription = "dumy")
+    }
+}
+
+
+@Composable
+fun ImageCom() {
+    Image(
+        painter = painterResource(id = R.drawable.image),
+        contentDescription = "my-image",
+        // colorFilter = ColorFilter.tint(Color.Blue)
+        contentScale = ContentScale.FillWidth
+    )
 }
 
 
@@ -42,18 +92,7 @@ fun TextCom(name: String = "Rejaul") {
         fontWeight = FontWeight.Bold,
         fontSize = 30.sp,
 
-    );
-}
-
-
-@Composable
-fun ImageCom () {
-   Image(
-       painter = painterResource(id = R.drawable.image) ,
-       contentDescription = "my-image",
-       // colorFilter = ColorFilter.tint(Color.Blue)
-        contentScale = ContentScale.FillWidth
-   )
+        );
 }
 
 
